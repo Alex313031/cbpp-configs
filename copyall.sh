c72ea0cdde381bea7f8cc475b9cce3acfb0cbfc8
Alexander Frick <Alex313031@gmail.com>
2025-09-28 03:21:04 -0500

    add initial sources

---
+++ copyall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cp -r -v ~/.config/jgmenu/* ./.config/jgmenu/ &&
+
+cp -r -v ~/.config/openbox/* ./.config/openbox/ &&
+
+cp -r -v ~/.config/tint2/tint2rc ./.config/tint2/ &&
+
+cp -r -v ~/.local/share/themes/* ./.local/share/themes/ &&
+
+exit 0
